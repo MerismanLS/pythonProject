a493549c302e75df6cdb4b2209c2e55ce3255132
Python#3 <Sanyacheck123@gmail.com>
2023-12-16 19:03:49 +0400

    68mly

--- Sprites/wall.py
+++ Sprites/wall.py
@@ -8,7 +8,7 @@ class Wall(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load("./assets/wall.png")
-        self.image = pygame.transform.scale(self.image, (48, 48))dd
+        self.image = pygame.transform.scale(self.image, (48, 48))
         self.rect = self.image.get_rect()
         self.x = 0
         self.y = 0
